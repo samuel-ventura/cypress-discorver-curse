7f301f593a49632bc1e40e74410475a6d4cbc1d8
Samuel Ventura <samuelventura.contato@gmail.com>
2022-07-20 16:28:08 -0300

    little change

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,6 @@
 describe('home page', () => {
     it('should be able to access the home page', () => {
-        cy.viewport(1920, 1080)
-        cy.visit('https://buger-eats.vercel.app')
+        cy.visit('/')
         cy.get('#page-home main h1').should('contain', 'Seja um parceiro entregador pela Buger Eats')
     })
 })
